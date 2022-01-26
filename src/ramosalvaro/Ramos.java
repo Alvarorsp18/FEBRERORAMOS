406be3cef74ba5e727c29dd6f060f217e67f8c54
dam <dam@203P20>
2022-01-26 09:45:58 +0100

    RAMOS Primer Commit

---
+++ src/ramosalvaro/Ramos.java
@@ -0,0 +1,17 @@
+package ramosalvaro;
+
+/**
+ *
+ * @author Alvaro Ramos Sainz
+ */
+public class Ramos {
+
+    /**
+     * @param args Lineas Main del código
+     */
+    public static void main(String[] args) {
+        String mensaje="Estas son unas líneas de prueba.";
+        System.out.print(mensaje);
+    }
+    
+}
