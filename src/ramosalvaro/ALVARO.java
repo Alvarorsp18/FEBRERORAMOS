9897af00ddef797c2833b2eccb208daf5ca5faef
dam <dam@203P20>
2022-01-26 09:55:05 +0100

    RAMOS Segundo Commit

---
+++ src/ramosalvaro/ALVARO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramosalvaro;
+
+/**
+ *
+ * @author dam
+ */
+public class ALVARO {
+    String mensaje;
+
+    public ALVARO(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
